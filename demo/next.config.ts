53a033774c26e3c73561a063a8a3f4ed4fdc4108
nxiiun <nxiiun@gmail.com>
2025-07-03 19:01:36 +1200

    update

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  basePath: '',
-  assetPrefix: '',
+  ...(isGithubPages
+    ? {
+        basePath: '/pvq-sdk',
+        assetPrefix: '/pvq-sdk/',
+      }
+    : {}),
 };
 
 export default nextConfig;
