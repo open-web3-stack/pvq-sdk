b16dfe30e72b5f8a34dfc9ae895f578e2889b055
openweb3 <openweb3@acala.com>
2025-07-29 01:31:14 +1200

    fix lint

--- demo/app/swap/layout.tsx
+++ demo/app/swap/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 export default function SwapLayout({
   children,
